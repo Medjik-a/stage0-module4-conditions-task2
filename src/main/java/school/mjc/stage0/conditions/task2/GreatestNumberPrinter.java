9bdb5add77438fec1b41fc815f74072adfae5dca
Medjik <med.akhmetov@mail.ru>
2023-07-15 20:56:26 +0600

    second attempt

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,10 +2,9 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        if(first>second){
+        if (first > second) {
             System.out.println(first);
-        }
-        else {
+        } else {
             System.out.println(second);
         }
     }
